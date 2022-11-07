ac962a3a6b4a120d9037e55c83acd7dee0380fa6
Rahul Kumar <rahulloni000@gmail.com>
2022-11-07 08:37:57 +0545

    2nd class

--- app/src/main/java/com/example/l6_1class/MainActivity.java
+++ app/src/main/java/com/example/l6_1class/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.l6_1class;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button login;
+    TextView signup;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login=findViewById(R.id.btn_login);
+        signup=findViewById(R.id.txt_signup);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent=new Intent(MainActivity.this,ForgotPassword.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent SignupIntent=new Intent(getApplicationContext(),Signup.class);
+                startActivity(SignupIntent);
+            }
+        });
     }
 }
